42a1c4d723a3eead86ce2a1862e99d99dc7f688b
Hunt Collings <hunt@collings.co.uk>
2018-12-17 19:33:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 
 function findMatching (drivers, string) {
-  let result = drivers.filter(x => x === string)  ;
+  let result = drivers.filter(x => x.toLowerCase === string)  ;
   return result;
 
 }
